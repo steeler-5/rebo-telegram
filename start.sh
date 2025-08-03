54f9fd34d3bb815564a8e379cec95b19fe476b4f
steeler-5 <beaualphonso@gmail.com>
2025-08-03 00:41:04 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
-
 echo "🔥 start.sh triggered"
-
-BOT_TOKEN=$TELEGRAM_BOT_TOKEN
-
-echo "🧪 Loaded TELEGRAM_BOT_TOKEN: $BOT_TOKEN"
-
-if [ -z "$BOT_TOKEN" ]; then
-  echo "❌ TELEGRAM_BOT_TOKEN is not set!"
-  exit 1
-fi
-
-echo "🔗 Telegram check:"
-curl -s "https://api.telegram.org/bot$BOT_TOKEN/getMe"
-
-echo "✅ Shell script complete"
+python main.py
